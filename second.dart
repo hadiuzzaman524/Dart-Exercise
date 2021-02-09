64447dce93dd3e40491989b47c2b80a5473a314d
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2021-02-09 20:12:40 +0600

    second class

---
+++ second.dart
@@ -0,0 +1,99 @@
+void main() {
+  /*
+  Variable dec.
+  1.The first character should not be a digit.
+  2.Special characters are not allowed except
+   underscore (_) or a dollar sign ($).
+  4.The first character must be
+  alphabet(uppercase or lowercase) or underscore.
+  5.Identifiers must be unique and cannot
+  contain whitespace.
+   */
+  /*int Up = 33;
+  // int Up=44;
+  print(Up);*/
+
+  //Comment same as c,c++,java -> single line comment
+  /*
+  multiline comment
+   */
+
+  //Data Types
+  /*
+  number-> int,double
+  string,bool,dynamic,var,map,list
+   */
+  /* int age=23;
+  print(age);
+  double gpa=3.45;
+  print(gpa);
+  String name="Oishi";
+  print(name);
+  bool flag=true;
+  print(flag);*/
+
+ /* var x = 33.44;
+  print(x.runtimeType);
+  var name = "My name is Oishi";
+  print(name);
+
+  dynamic y = 44;
+  print(y.runtimeType);
+
+  dynamic doublevalue = 33.454;
+  print(doublevalue);
+  String heart_symbol = '\u2665';
+  print(heart_symbol.runtimeType);
+  print(heart_symbol);
+*/
+
+// Type conversion
+/*  var mark="5534";
+  print(mark.runtimeType);
+
+  var changetype=int.parse(mark);
+  print(changetype.runtimeType);
+
+  var changetype2=double.parse(mark);
+  print(changetype2.runtimeType);
+
+  var cgpa="3.76";
+  var changeCgpa=num.parse(cgpa);
+  print(changeCgpa.runtimeType);
+
+   var cgpa2="3";
+  var changeCgpa2=num.parse(cgpa2);
+  print(changeCgpa2.runtimeType);
+
+  var subject=3210;
+  String changesubject=subject.toString();
+  print(changesubject.runtimeType);
+
+  String lower='abcdefghijklmnopqrstuvwxyz';
+  print(lower.toUpperCase());
+
+  //trim
+
+  String trims=" oishi ";
+  //print original length
+  print(trims.length);
+  //print new length
+  print(trims.trim().length);
+  print(trims.trimLeft().length);
+  print(trims.trimRight().length);*/
+
+
+  /*
+  const and final keyword
+   */
+
+  const x=234;
+  // x=44;
+
+  final y=4423;
+  //
+
+
+
+
+}
