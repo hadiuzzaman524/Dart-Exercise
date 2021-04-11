3aed991cb3f5651a0201a7630c0aea946271ebe5
Md. Hadiuzzaman <52348628+hadiuzzaman524@users.noreply.github.com>
2021-04-11 20:10:12 +0600

    done final lec

--- Future/first.dart
+++ Future/first.dart
@@ -1,8 +1,21 @@
 
-void getRequest(String x){
-
+Future<void> getRequest(String x) async{
+ await Future.delayed(Duration(seconds: 5));
+  print(x);
 }
 
-void main(){
+void main() async{
 
+  print("Requesting.........");
+/*
+ await getRequest("www.google.com");
+
+
+*/
+
+  getRequest("Google.com").then((value){
+    print("Im working ");
+  });
+
+  print("Request Complete");
 }
